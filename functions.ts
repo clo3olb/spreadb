742849976db160f3c0eb603cd73e59e9dbdfd8c1
clo3olb <clo3olb@gmail.com>
2022-12-11 21:51:01 +0800

    add backup function

---
+++ functions.ts
@@ -0,0 +1,7 @@
+function backUp() {
+  const fileCopy = DriveApp.getFileById(DB_SHEETS_ID).makeCopy();
+  const today = new Date();
+
+  fileCopy.setName(`데이터베이스 백업 - ${today.toString()}`);
+  DriveApp.getFolderById(BACKUP_FOLDER_ID).addFile(fileCopy);
+}
