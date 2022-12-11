0be1d860f0171b36b7ab861899c80c7341bf7f35
clo3olb <clo3olb@gmail.com>
2022-12-11 21:51:35 +0800

    change filename

---
+++ z_index.ts
@@ -0,0 +1,7 @@
+const DB_SHEETS_ID = "1OR52xHhhuTSLqXALdw2VQ-_tVfsi1pXvOhlI2EoArK8";
+const BACKUP_FOLDER_ID = "10kQ4ijhzV02JqNW7tYunuR-pWYK9w5Qb";
+
+function main() {
+  const today = new Date();
+  Logger.log(today.toLocaleString());
+}
